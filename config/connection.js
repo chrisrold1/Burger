8145597bfe8d2c2d47a41a00738ed7c6f6c9b575
chrisrold senora <youexample.com>
2019-01-26 14:41:52 -0600

    database added

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
 else{
   connection = mysql.createConnection({
     host: 'localhost',
-    port: 8080,
+    port: 3306,
     user : 'root',
     password :'NeilaSenor@2', //password
     database : 'burger_db'    //database
--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,20 @@
 DROP DATABASE IF EXIST burger_db;
 
 --Create DAatabase
-CREATE DATABASE burger_db;
+
+CREATE DATABASE jtbgu3odqymd6asa_db;
+
+--Use Databse 
+USE DATABASE jtbgu3odqymd6asa_db;
+
+--Create Table
+CREATE TABLE burgers
+(
+     id int NOT NULL AUTO_INCREMENT,
+     burger_name VARCHAR(255) NOT NULL,
+     devoured boolean NOT NULL,
+     PRIMARY KEY (id)
+);
 
 --Use Databse 
 USE DATABASE burger_db;
