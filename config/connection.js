65f458dea65b39296e5f276dc4758df7f668b339
chrisrold senora <youexample.com>
2019-01-26 14:02:42 -0600

    added jaw

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,11 @@ if (process.env.JAWSDB_URL) {
 else{
   connection = mysql.createConnection({
     host: 'localhost',
+    port: 8080,
     user : 'root',
     password :'NeilaSenor@2', //password
     database : 'burger_db'    //database
-  })
+  });
 }
 
 connection.connect(function(err) {
